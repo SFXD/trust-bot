a81a7983d69ca9f11368a06724d7da7dacc1c605
George Doenlen <gdoenlen@gmail.com>
2020-03-31 13:12:55 -0400

    add ebean migration tasks

---
+++ src/main/java/com/github/sfxd/trust/model/migration/GenerateDbMigration.java
@@ -0,0 +1,19 @@
+package com.github.sfxd.trust.model.migration;
+
+import java.io.IOException;
+
+import io.ebean.annotation.Platform;
+import io.ebean.dbmigration.DbMigration;
+
+class GenerateDbMigration {
+
+    public static void main(String[] args) throws IOException {
+        System.setProperty("ddl.migration.version", "1.0");
+        System.setProperty("ddl.migration.name", "init");
+
+        var migration = DbMigration.create();
+        migration.setPlatform(Platform.H2);
+
+        migration.generateMigration();
+    }
+}
