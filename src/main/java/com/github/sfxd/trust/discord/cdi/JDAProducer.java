e5819a4d9ee26eda0052a7d25f6f9b0e0c29660d
George Doenlen <gdoenlen@gmail.com>
2025-10-22 11:08:49 -0700

    remove wrong comments

--- src/main/java/com/github/sfxd/trust/discord/cdi/JDAProducer.java
+++ src/main/java/com/github/sfxd/trust/discord/cdi/JDAProducer.java
@@ -15,9 +15,6 @@ import net.dv8tion.jda.api.JDABuilder;
 public class JDAProducer {
     private static final Logger LOGGER = LoggerFactory.getLogger(JDAProducer.class);
 
-    // This MUST be @Singleton scoped!!
-    // Weld's proxying will cause JDA to not properly register
-    // slash commands since the calls will be made on the proxy object.
     @Bean(initMethod = "awaitReady", destroyMethod = "shutdown")
     public JDA produceJDA(MessageListener listener) throws Exception {
         var jda = JDABuilder.createDefault(System.getProperty("trust.discord.token"))
